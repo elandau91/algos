773008fee5991e62a4cb25198ee0152ddd4215a8
elandau91 <eli.b.landau@gmail.com>
2020-10-17 10:39:02 -0400

    note update

--- probsolve/patternNotes.js
+++ probsolve/patternNotes.js
@@ -10,4 +10,10 @@
         //Creating "pointers" or values that correspong to an index or postion and move
         //towards either the beginning, middle or end based on problem.
         //Very good for space complexity.
+
+    //"Sliding Window"
+        //This pattern involves creating a window which can either be an array or a number
+        //from one position to another.
+        //Depending on a certain condition, the window either increases or closes
+        //Very useful for keeping track of data in a an array/string
         
