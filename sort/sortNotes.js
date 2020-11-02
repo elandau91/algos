f20cf32b8bfddbc4399435d233927650285cfca9
elandau91 <eli.b.landau@gmail.com>
2020-11-02 14:22:54 -0500

    merge sort

--- sort/sortNotes.js
+++ sort/sortNotes.js
@@ -9,6 +9,12 @@
             //If it returns a negative number, a comes before b
             //If it returns a postive number, a should come after b
             //If it returns 0, a and b will be considered the same as far as sort is concerned
+
+    
+    //General Notes
+        //Elementary Algos work well on small data sets (20 items), but don't scale well when data gets larger (10,000 items)
+        //Intermediate Algos work well for big data sets.
+        //Intermediate Algos improve sort time from O(n) to O(n log n)
         
 
 //QUADRATIC OR ELEMENTARY ALGORITHMS
@@ -44,3 +50,13 @@
         //We can do better ;)
 
 
+//CRAZIER OR INTERMEDIATE SORTING ALGORITHMS 
+    //MERGE SORT
+        //Sorts by breaking splitting items into arrays of 1 or 0 since those are automatically sorted.
+        //Then it joins items into new arrays merging them together
+        //Big O
+            //Always O(n log n)
+            //No matter the data, it does everything the exact same way
+                //It's n log n, because it's logarithmic, since it needs to split and recombined.
+                    //Each split and join is a division/multiplication by 2. A logarithm
+        //This keeps track of decomposition and recomposition 
