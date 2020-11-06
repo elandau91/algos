cd21d98a7a13306a0335af5ec53346fc6c60ca01
elandau91 <eli.b.landau@gmail.com>
2020-11-06 17:59:14 -0500

    tree notes start

---
+++ dataStructures/trees/treeNotes.js
@@ -0,0 +1,25 @@
+//TREES
+
+    //Trees are data structures that consist of nodes in a parent/child relationship
+    //Lists are linear, Trees are non-linear, with multiple paths
+        //Singly linked lists are like a special case of a tree
+            //But if you add other branches, no longer a tree
+    //Tree nodes can only point from parent to child, not from sibling to sibling (that's a graph)
+    //Trees also have just ONE root entry point.
+        //Root is top node in a tree
+        //Child is a node directly connected to another node when moving away from the root
+        //Parent is the converse of child
+        //Siblings are groups of nodes with the same parent
+        //Leaf is a node with no children
+        //Edge is the connection between one node to another
+    
+    //Trees are common! They are seen:
+        //HTML: The DOM. HTML elements on the DOM are structured in a tree
+        //Network Routing: Network delivery schemes (broadcast, multicast, etc)
+        //Abstract Syntax Trees: The structure of languages (while loop, parent of conditional, etc)
+        //Artificial Intelligence: structuring decisions, etc
+        //Folders in OS: You have folders with files in them, and other folders, like on your Mac, etc
+        //JSON: Can be represented in tree structure
+        
+    //Types of Trees
+        //Binary Trees
