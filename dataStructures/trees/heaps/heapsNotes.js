76d52d8ff7bb6981a6e05523941bd8047ed15651
elandau91 <eli.b.landau@gmail.com>
2020-11-11 14:01:15 -0500

    heap intro

---
+++ dataStructures/trees/heaps/heapsNotes.js
@@ -0,0 +1,11 @@
+//HEAPS
+
+    //Binary Heaps
+        //Very similar to Binary Search Tree but it has different rules
+        //In a MaxBinaryHeap parent nodes are always larger than child nodes
+        //In a MinBinaryHeap parent nodes are always smaller than child nodes
+            //No order, but top to bottom differences
+        //No guarantees between sibling in either case
+        //Binary heaps are compact as possible, so every left and right is filled before moving down
+    //Binary Heaps are used to implement priority queues, which are very common data structures
+    //Binary Heaps are also used with Graph Traversal
