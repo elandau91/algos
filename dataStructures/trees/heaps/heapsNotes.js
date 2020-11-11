8c0fe4c2f4e7c3d02508497162992681e318dfda
elandau91 <eli.b.landau@gmail.com>
2020-11-11 17:25:53 -0500

    heaps insert

--- dataStructures/trees/heaps/heapsNotes.js
+++ dataStructures/trees/heaps/heapsNotes.js
@@ -9,3 +9,7 @@
         //Binary heaps are compact as possible, so every left and right is filled before moving down
     //Binary Heaps are used to implement priority queues, which are very common data structures
     //Binary Heaps are also used with Graph Traversal
+
+    //Easy way build binary heaps with array
+        //Finding Children: take index, multiple by 2, add 1 and 2 to get respective children of that index.
+        //Find Parent: Subtract 1, divide by 2, and Math.floor to get right index
