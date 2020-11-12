1870793112bb572577d57d59b87b35f6b9fde722
elandau91 <eli.b.landau@gmail.com>
2020-11-12 16:33:12 -0500

    priority queues with heaps

--- dataStructures/trees/heaps/heapsNotes.js
+++ dataStructures/trees/heaps/heapsNotes.js
@@ -13,10 +13,25 @@
     //Easy way build binary heaps with array
         //Finding Children: take index, multiple by 2, add 1 and 2 to get respective children of that index.
         //Find Parent: Subtract 1, divide by 2, and Math.floor to get right index
-        //Procudure for insert
+        //Procedure for insert
             //Insert element and bubble up accordingly
-        //Procude for removal
+        //Procedure for removal
             //Take from root
             //Replace root with lowest value
             //Then you "sink down" accordingly (opposite of bubble up)
+    
+    //Building a priority Queue
+        //A priority queue is a data strcuture when each element has a priority associated with it.
+            //Elements with higher priorities are served before elements with lower priorities
+        //Unix or Linux OS has "nice" ranking which is essentially a priority meter, for which tasks are worked on.
+    //Naive Version
+        //Put priority order in array
+        //Not a very good approach, since we'd have to compare every item each time
+        //Better to use Heap
+    //Using a Heap
+        //Bubbling up priority works perfectly for this
+        //Heaps have time complexity of log n much faster than array
+        //Priority queues are commonly associated and confused with heaps (since they are structred this way often)
+
+        
         
--- dataStructures/trees/heaps/maxBinaryHeap.js
+++ dataStructures/trees/heaps/maxBinaryHeap.js
@@ -2,7 +2,7 @@
 
 class MaxBinaryHeap{
     constructor(){
-        this.values = [41,39,33,18,27,12]
+        this.values = []
     }
 
     insert(element){
