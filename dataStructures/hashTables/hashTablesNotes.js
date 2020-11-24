7103eae1e60e051af92a85ab91233b32f8550cc7
elandau91 <eli.b.landau@gmail.com>
2020-11-24 11:09:16 -0500

    intro to hash tables

---
+++ dataStructures/hashTables/hashTablesNotes.js
@@ -0,0 +1,15 @@
+//Hash Tables (also known as Hash Map)
+
+    //What is a Hash Table?
+        //Hash Tables are used to store key/value pairs
+        //They are like arrays but the keys are not ordered
+        //They are fast! for finding, removing, searching
+    //All languages use them
+        //If data doesn't need on order, hash maps are preferrable
+        //Python has dictionaries
+        //JS has objects and maps
+        //Java, Go, & Scala have Maps
+        //Ruby has Hashes
+    
+    //This section is focused on building hashes, or objects in JS
+    
